04561d2c4a3d38b16c20e78f52e6c422361cc9c3
tareksalem <tarekpopp@gmail.com>
2017-11-18 21:05:27 +0200

    six

--- app.js
+++ app.js
@@ -1,40 +1,17 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require("express-session");
-const flash = require("connect-flash");
-const hbs = require("express-handlebars");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-var expressValidator = require("express-validator");
-const handlebars = require("handlebars");
-const momentjs = require("helper-moment");
-const multer = require("multer");
-const sanitize = require("sanitize-html");
-handlebars.registerHelper("moment", require("helper-moment"));
-const mongodb = require("mongodb");
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://tarek:tareksalem1@ds159235.mlab.com:59235/mongotarek");
-//mongoose.connect("localhost:27017/school");
-const db = mongoose.connection;
-const index = require('./routes/index');
-const users = require('./routes/controllers/users');
-require("./config/assistant");
-const storage = multer.diskStorage({
-    destination: "../public/images",
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
-    }
-});
-const app = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -42,41 +19,11 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// use session module
-app.use(session({
-    secret: "max",
-    saveUninitialized: true,
-    resave: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(expressValidator({
-    errorFormatter: function(param, msg, value) {
-        var namespace = param.split('.')
-            , root    = namespace.shift()
-            , formParam = root;
-
-        while(namespace.length) {
-            formParam += '[' + namespace.shift() + ']';
-        }
-        return {
-            param : formParam,
-            msg   : msg,
-            value : value
-        };
-    }
-}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(flash());
-app.use(function (req, res, next) {
-    res.locals.message = require("express-messages")(req, res);
-    res.locals.login = req.isAuthenticated();
-    res.locals.notlogin = !req.isAuthenticated();
-    next();
-});
+
 app.use('/', index);
-app.use('/controllers', users);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
